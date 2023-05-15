8035521ae514546eb22da472e129621aafa5113e
rabbitSyrup <aswxec@gmail.com>
2023-05-15 17:41:38 +0900

    no audio

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import Phaser from 'phaser';
 import { MainScene } from './scene/mainScene';
 
 const config: Phaser.Types.Core.GameConfig = {
-
   type: Phaser.AUTO,
   width: 160,
   height: 160,
   backgroundColor: 0x000,
   scene: [MainScene],
+  audio: {
+    noAudio: true
+  },
   physics: {
     default: 'arcade',
     arcade: {
